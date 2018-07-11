4af0e3cd58e3169305fbc4a7d41c57fa4e9a1eb4
Paretkf <paretk7@gmail.com>
2018-07-11 19:45:32 +0700

    get go lang wc

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"strings"
+
+	"golang.org/x/tour/wc"
 )
 
-func main() {
-	m := map[string]int{
-		"number1": 10,
+func WordCount(s string) map[string]int {
+	a := strings.Fields(s)
+	b := make(map[string]int)
+	for i := 0; i < len(a); i++ {
+		_, ok := b[string(a[i])]
+		if ok {
+			b[string(a[i])] = b[string(a[i])] + 1
+		} else {
+			b[string(a[i])] = 1
+		}
 	}
-	fmt.Println(m["number1"])
+	return b
+}
+
+func main() {
+	wc.Test(WordCount)
 }
